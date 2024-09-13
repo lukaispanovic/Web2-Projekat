dc74b1eb93623c7f3aa4428542a3313d453fa5d4
LukaIspanovic <ispanovic.luka@gmail.com>
2024-09-13 21:11:31 +0200

    Dodat osnovni datamodel i servis projekti za korisnika i voznju

---
+++ Backend App/Common/DataModel/Ride.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DataModel
+{
+    public class Ride
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public int UserId { get; set; }
+
+        public int? DriverId { get; set; }
+
+        [DataMember]
+        public string StartAddress { get; set; } = string.Empty;
+
+        [DataMember]
+        public string EndAddress { get; set; } = string.Empty;
+
+        [DataMember]
+        public float Price { get; set; }
+
+        [DataMember]
+        public int WaitingTime { get; set; }
+
+        [DataMember]
+        public int? TravelTime { get; set; }
+
+        [DataMember]
+        public bool RideStatus { get; set; }
+
+        [DataMember]
+        public int ReviewScore { get; set; }
+    }
+}
---
+++ Backend App/Common/DataModel/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DataModel
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public long Id { get; set; }
+
+        [DataMember]
+        public string Username { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        public string Address { get; set; }
+
+        [DataMember]
+        public DateTime Birthday { get; set; }
+
+        [DataMember]
+        public string UserType { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string ProfilePictureUrl { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public int? RideDataId { get; set; }
+
+        [DataMember]
+        public bool? Verified { get; set; }
+    }
+}
